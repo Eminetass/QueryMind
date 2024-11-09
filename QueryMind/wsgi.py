f8d816b494c182befb49ad15ca02eb56dddbfa7f
Emine Tas <eminetas126@gmail.com>
2024-11-10 02:20:43 +0300

    improvements

--- QueryMind/wsgi.py
+++ QueryMind/wsgi.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from api.routers.query_mind import router
+from api.routers.query_mind import router 
 
 def init_routers(app: FastAPI) -> None:
     app.include_router(router)
